7d0f59f9fd1605d2a246331c72e23aad81a21d0e
Julien879465 <beillonnet@laposte.net>
2023-03-16 12:31:43 +0100

    searchbar

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { useState } from "react";
+import searchIcon from "../assets/Icons/search.svg";
 
 function SearchBar() {
-  return <div>SearchBar</div>;
+  const [searchValue, setSearchValue] = useState("");
+  console.info(searchValue);
+  return (
+    <div className="relative w-full">
+      <input
+        className="absolute h-[63px] w-[350px] border-solid border-2 border-grey3 px-4 rounded-full"
+        type="text"
+        placeholder="Entrer votre recherche"
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+
+      <img
+        className="absolute h-[24px] top-5 left-[280px]"
+        src={searchIcon}
+        alt=""
+      />
+    </div>
+  );
 }
 
 export default SearchBar;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,10 @@
+import SearchBar from "../components/SearchBar";
+
 export default function Home() {
   return (
     <div>
       <p>Home</p>
+      <SearchBar />
     </div>
   );
 }
