d82c5dad6611aae6a57eaa4ba53ad8c23850e7bf
clauga <guerrero.ayala.cs@gmail.com>
2023-03-16 23:31:18 +0100

    final details phone page + FAQ

--- frontend/src/components/FaqQuestions.jsx
+++ frontend/src/components/FaqQuestions.jsx
@@ -15,20 +15,23 @@ function FaqQuestions() {
             </h1>
             <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
               Les questions les plus courantes sur le fonctionnement de notre
-              entreprise et ce que peut faire pour vous.
+              association et ce que peut faire pour vous.
             </p>
           </div>
           <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
             <div className="w-full lg:w-1/2 px-4 py-2">
               <details className="mb-4">
                 <summary className="font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                  Comment puis-je entrer un téléphone pour être classifié?
+                  Comment fonctionne la categorisation des smarthphone?
                 </summary>
 
-                <span>
-                  Laboris qui labore cillum culpa in sunt quis sint veniam.
-                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                  minim velit nostrud pariatur culpa magna in aute.
+                <span className="text-justify ">
+                  <br />
+                  La catégorisation des smartphones d'occasion fonctionne selon
+                  plusieurs critères pour aider les techniciens à trouver le
+                  prix juste et qui convient le mieux aux besoins. Avec les
+                  critères suivants : marque, modèle, RAM, stockage, indice
+                  Antutu et réseau.
                 </span>
               </details>
               <details className="mb-4">
@@ -36,7 +39,8 @@ function FaqQuestions() {
                   Comment fonctionne le système emmaüs connect?
                 </summary>
 
-                <span>
+                <span className="text-justify ">
+                  <br />
                   Dans notre magasin de téléphones portables d'occasion, nous
                   sommes heureux de vous informer que les téléphones que nous
                   avons révélés ont été donnés et ont été dûment évalués et
@@ -51,10 +55,11 @@ function FaqQuestions() {
               </details>
               <details className="mb-4">
                 <summary className="mb-4 font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                  Puis-je saisir un fichier en csv ?
+                  Puis-je saisir un fichier en csv pour saisir les informations?
                 </summary>
 
-                <span>
+                <span className="text-justify max-w-sm leading-relaxed">
+                  <br />
                   Bien sûr! En plus de saisir manuellement les informations de
                   chaque téléphone dans la calculatrice virtuelle et de générer
                   un code QR pour chacun, il est également possible de
@@ -69,35 +74,70 @@ function FaqQuestions() {
             <div className="w-full lg:w-1/2 px-4 py-2">
               <details className="mb-4">
                 <summary className="font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                  Puis-je saisir un fichier en csv ?
+                  Quelle est la configuration minimale pour mettre un smartphone
+                  en vente ?
                 </summary>
 
                 <span className="px-4 py-2">
-                  Laboris qui labore cillum culpa in sunt quis sint veniam.
-                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                  minim velit nostrud pariatur culpa magna in aute.
+                  <br />
+                  Android: 8 <br /> Ram: 2GO <br />
+                  Mémoire: 16GO
+                  <br /> écran: 4P
+                  <br /> Réseau: 4G <br />
+                  avec chargeur et cable
                 </span>
               </details>
               <details className="mb-4">
                 <summary className="font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                  Puis-je saisir un fichier en csv ?
+                  Est-il possible de générer un QR code après avoir saisi un
+                  téléphone ?
                 </summary>
 
-                <span className="px-4 py-2">
-                  Laboris qui labore cillum culpa in sunt quis sint veniam.
-                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                  minim velit nostrud pariatur culpa magna in aute.
+                <span className="mt-6 px-4 py-2">
+                  <br />
+                  Il est possible de générer un code QR qui reprend toutes les
+                  informations concernant le téléphone. Ce code QR peut être
+                  utilisé pour identifier et suivre chaque téléphone, ce qui est
+                  particulièrement utile pour suivre les téléphones donnés et
+                  vendus à l'avenir.
                 </span>
               </details>
               <details className="mb-4">
-                <summary className="font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                  Puis-je saisir un fichier en csv ?
+                <summary className="mb-6 font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                  Comment puis-je utiliser la calculatrice virtuelle?
                 </summary>
 
                 <span className="px-4 py-2">
-                  Laboris qui labore cillum culpa in sunt quis sint veniam.
-                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                  minim velit nostrud pariatur culpa magna in aute.
+                  <br />
+                  1-.Marque: Tout d'abord, vous devez trier les smartphones
+                  d'occasion en fonction de leur marque. Pour cela, vous pouvez
+                  simplement regrouper les téléphones en fonction de la marque,
+                  par exemple, Apple, Samsung, Huawei, Xiaomi, etc. <br />{" "}
+                  2-.Modèle: Ensuite, une fois que vous avez regroupé les
+                  téléphones en fonction de leur marque, vous devez les classer
+                  par modèle. Pour cela, vous pouvez utiliser les noms de
+                  modèles de chaque marque pour trier les téléphones, par
+                  exemple, iPhone X, Samsung Galaxy S20, Huawei P30, Xiaomi Mi
+                  10, etc. <br /> 3-.RAM: La RAM est un facteur important à
+                  considérer lors de l'achat d'un smartphone, car elle détermine
+                  la vitesse et les performances globales du téléphone. Vous
+                  pouvez classer les téléphones en fonction de leur quantité de
+                  RAM, par exemple, 2 Go, 4 Go, 6 Go, 8 Go, etc. <br />{" "}
+                  4-.Stockage: Le stockage est également un facteur important à
+                  considérer lors de l'achat d'un smartphone, car il détermine
+                  la quantité de données et d'applications que vous pouvez
+                  stocker sur votre téléphone. Vous pouvez classer les
+                  téléphones en fonction de leur capacité de stockage, par
+                  exemple, 32 Go, 64 Go, 128 Go, 256 Go, etc. <br /> 5-.Indice
+                  Antutu: L'indice Antutu est un score de référence qui mesure
+                  les performances globales d'un smartphone. Vous pouvez classer
+                  les téléphones en fonction de leur indice Antutu, en allant
+                  des scores les plus élevés aux scores les plus bas. <br />{" "}
+                  6-.Réseau: Enfin, le réseau est également un facteur important
+                  à considérer lors de l'achat d'un smartphone, car il détermine
+                  la vitesse et la qualité des connexions Internet et mobiles.
+                  Vous pouvez classer les téléphones en fonction de leur
+                  capacité de réseau, par exemple, 4G, 5G, etc.
                 </span>
               </details>
             </div>
--- frontend/src/components/PhoneCards.jsx
+++ frontend/src/components/PhoneCards.jsx
@@ -59,22 +59,9 @@ function PhoneCards({ brand, ram, storage, url, network, model }) {
           <div className="flex items-center">
             <button
               type="button"
-              className="text-white bg-main hover:bg-main-light focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-7 py-2 text-center inline-flex items-center "
+              className="bg-main hover:bg-main-light text-white py-2 px-4 rounded-full "
             >
               Voir plus
-              <svg
-                aria-hidden="true"
-                className="w-5 h-5 ml-2 -mr-1"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
             </button>
           </div>
 
