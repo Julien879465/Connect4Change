897b2826c5d4275003aa2d7287e326b92d207616
Julien879465 <beillonnet@laposte.net>
2023-03-16 14:26:21 +0100

    fix

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,22 +1,15 @@
 import { useState } from "react";
-import searchIcon from "../assets/Icons/search.svg";
 
 function SearchBar() {
   const [searchValue, setSearchValue] = useState("");
   console.info(searchValue);
   return (
-    <div className="relative w-full">
+    <div>
       <input
-        className="absolute h-[63px] w-[350px] border-solid border-2 border-grey3 px-4 rounded-full"
+        className="h-[63px] w-[350px] border-solid font-medium border-2 text-lg border-grey3 focus:outline-grey2 px-4 rounded-full"
         type="text"
         placeholder="Entrer votre recherche"
         onChange={(e) => setSearchValue(e.target.value)}
-      />
-
-      <img
-        className="absolute h-[24px] top-5 left-[280px]"
-        src={searchIcon}
-        alt=""
       />
     </div>
   );
--- frontend/src/pages/PhonePage.jsx
+++ frontend/src/pages/PhonePage.jsx
@@ -1,18 +1,25 @@
 import React from "react";
+import SearchBar from "../components/SearchBar";
 import deco from "../assets/Images/Deco.png";
 import PhoneCards from "../components/PhoneCards";
 
 function PhonePage() {
   return (
     <div className="m-5">
-      <div className=" font-bold text-2xl font-jost flex items-center gap-4">
-        <span>Téléphones</span>
-        <img
-          src={deco}
-          alt="Descripción de la imagen"
-          className="w-3 h-4 mb-3"
-        />
+      <div className=" font-bold text-2xl flex items-center justify-between gap-4">
+        <div className="flex">
+          <span>Téléphones</span>
+          <img
+            src={deco}
+            alt="Descripción de la imagen"
+            className="w-3 h-4 mb-3"
+          />
+        </div>
+        <div className="flex mr-10">
+          <SearchBar />
+        </div>
       </div>
+
       <div className="text-grey2 text-xs mb-8">Jeudi 16 mars 2023</div>
       <PhoneCards />
       <PhoneCards />
