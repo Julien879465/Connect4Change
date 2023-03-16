f8f83aa973017a712ca1f3e012aa8d366067f9bd
clauga <guerrero.ayala.cs@gmail.com>
2023-03-16 11:35:01 +0100

    faq + phone page+ description page front finished

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import FAQ from "./pages/FAQ";
 import Home from "./pages/Home";
 import OnePhone from "./pages/OnePhone";
 import PhonePage from "./pages/PhonePage";
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/Phonepage" element={<PhonePage />} />
       <Route path="/onephone" element={<OnePhone />} />
+      <Route path="/faq" element={<FAQ />} />
     </Routes>
   );
 }
