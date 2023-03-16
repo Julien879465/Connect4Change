1cd9cfc3da10691fee0f0e7fcc50bf3824533752
clauga <guerrero.ayala.cs@gmail.com>
2023-03-16 21:15:50 +0100

    phones page + description page done, please leave me push!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
-//PAGES
+// PAGES
 import Home from "./pages/Home";
 import Connexion from "./pages/Connexion";
 import Registration from "./pages/Registration";
@@ -11,10 +11,10 @@ import OnePhone from "./pages/OnePhone";
 import Telechargements from "./pages/Telechargements";
 import FAQ from "./pages/FAQ";
 
-//COMPONENTS
+// COMPONENTS
 import SideBar from "./components/SideBar";
 
-//CSS
+// CSS
 import "react-toastify/dist/ReactToastify.min.css";
 
 function App() {
