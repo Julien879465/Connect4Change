ddb28a00362c1c11aff1a558f05170863da2056a
Cassandra Blondez <[cassandra.blondez@gmail.com](mailto:cassandra.blondez@gmail.com)>
2023-03-16 12:37:18 +0100

    delete buttonsConnexion

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,13 @@
-import ButtonsConnexion from "./ButtonsConnexion";
 import logo from "../assets/Images/LogoConnect.png";
+import DeconnexionButton from "./DeconnexionButton";
+import { useCurrentUserContext } from "../contexts/CurrentUserContext";
 
 function Navbar() {
+  const { user } = useCurrentUserContext();
   return (
     <div className="flex flex-row items-center justify-between shadow-nav">
       <img src={logo} alt="logo connect4change" className="max-w-[12%] p-2" />
-      <ButtonsConnexion />
+      {user && <DeconnexionButton />}
     </div>
   );
 }
