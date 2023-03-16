977c63fd25132e348449df5156ae5d2f920bad93
Cassandra Blondez <[cassandra.blondez@gmail.com](mailto:cassandra.blondez@gmail.com)>
2023-03-16 11:07:27 +0100

    add logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import ButtonsConnexion from "./ButtonsConnexion";
+import logo from "../assets/Images/LogoConnect.png";
 
 function Navbar() {
   return (
     <div>
+      <img src={logo} alt="logo connect4change" />
       <ButtonsConnexion />
     </div>
   );
