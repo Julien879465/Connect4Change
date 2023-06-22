69a38fbe600c20b02234f19ff8791720a9cfa73b
Julien879465 <beillonnet@laposte.net>
2023-06-22 15:26:09 +0200

    change href and remove some href on phoneresult

--- frontend/src/components/PhoneResult.jsx
+++ frontend/src/components/PhoneResult.jsx
@@ -5,7 +5,7 @@ function PhoneResult() {
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 w-full mb-8">
       <div className="relative mx-auto w-full">
         <a
-          href="/OnePhone"
+          href="/telephones/1"
           className="relative inline-block duration-300 ease-in-out transition-transform transform hover:-translate-y-2 w-full"
         >
           <div className="shadow p-4 rounded-lg bg-white">
@@ -69,195 +69,147 @@ function PhoneResult() {
       </div>
 
       <div className="relative mx-auto w-full">
-        <a
-          href="/"
-          className="relative inline-block duration-300 ease-in-out transition-transform transform hover:-translate-y-2 w-full"
-        >
-          <div className="shadow p-4 rounded-lg bg-white">
-            <div className="flex justify-center relative rounded-lg overflow-hidden h-90">
-              <div className="transition-transform duration-500 transform ease-in-out hover:scale-110 w-full">
-                <img
-                  src="https://www.indicereparabilite.fr/wp-content/uploads/2022/01/8-a-10-5.png"
-                  alt="Descripción de la imagen"
-                />
-                <div className="absolute inset-0 bg-black opacity-10" />
-              </div>
-
-              <div className="absolute flex justify-center bottom-0 mb-3">
-                <div className="flex bg-white px-4 py-1 space-x-5 rounded-lg overflow-hidden shadow">
-                  <p className="flex items-center font-medium text-gray-800">
-                    5G
-                  </p>
-
-                  <p className="flex items-center font-medium text-gray-800">
-                    GPS
-                  </p>
-
-                  <p className="flex items-center font-medium text-gray-800">
-                    LCD
-                  </p>
-                </div>
-              </div>
-
-              <span className="absolute top-0 left-0 inline-flex mt-3 ml-3 px-3 py-2 rounded-lg z-10 bg-green text-sm font-medium text-white select-none">
-                Disponible
-              </span>
+        <div className="shadow p-4 rounded-lg bg-white">
+          <div className="flex justify-center relative rounded-lg overflow-hidden h-90">
+            <div className="transition-transform duration-500 transform ease-in-out hover:scale-110 w-full">
+              <img
+                src="https://www.indicereparabilite.fr/wp-content/uploads/2022/01/8-a-10-5.png"
+                alt="Descripción de la imagen"
+              />
+              <div className="absolute inset-0 bg-black opacity-10" />
             </div>
 
-            <div className="text-center block mt-4">
-              <h2
-                className="font-medium text-base md:text-lg text-gray-800 line-clamp-1"
-                title="New York"
-              >
-                Smartphone Athesi RT55
-              </h2>
-              <p
-                className="mt-2 text-sm text-yellow line-clamp-1"
-                title="New York, NY 10004, United States"
-              >
-                INDICE DE RÉPARABILITÉ SMARTPHONE
-              </p>
-            </div>
+            <div className="absolute flex justify-center bottom-0 mb-3">
+              <div className="flex bg-white px-4 py-1 space-x-5 rounded-lg overflow-hidden shadow">
+                <p className="flex items-center font-medium text-gray-800">
+                  5G
+                </p>
 
-            <div className="mt-8">
-              <div className="flex items-center">
-                <button
-                  type="button"
-                  className="bg-main hover:bg-main-light text-white py-2 px-4 rounded-full "
-                >
-                  Voir plus
-                </button>
+                <p className="flex items-center font-medium text-gray-800">
+                  GPS
+                </p>
+
+                <p className="flex items-center font-medium text-gray-800">
+                  LCD
+                </p>
               </div>
             </div>
+
+            <span className="absolute top-0 left-0 inline-flex mt-3 ml-3 px-3 py-2 rounded-lg z-10 bg-grey2 text-sm font-medium text-white select-none">
+              Rupture de stock
+            </span>
           </div>
-        </a>
+
+          <div className="text-center block mt-4">
+            <h2
+              className="font-medium text-base md:text-lg text-gray-800 line-clamp-1"
+              title="New York"
+            >
+              Smartphone Athesi RT55
+            </h2>
+            <p
+              className="mt-2 text-sm text-yellow line-clamp-1"
+              title="New York, NY 10004, United States"
+            >
+              INDICE DE RÉPARABILITÉ SMARTPHONE
+            </p>
+          </div>
+        </div>
       </div>
       <div className="relative mx-auto w-full">
-        <a
-          href="/"
-          className="relative inline-block duration-300 ease-in-out transition-transform transform hover:-translate-y-2 w-full"
-        >
-          <div className="shadow p-4 rounded-lg bg-white">
-            <div className="flex justify-center relative rounded-lg overflow-hidden h-90">
-              <div className="transition-transform duration-500 transform ease-in-out hover:scale-110 w-full">
-                <img
-                  src="https://www.indicereparabilite.fr/wp-content/uploads/2022/01/8-a-10-2.png"
-                  alt="Descripción de la imagen"
-                />
-                <div className="absolute inset-0 bg-black opacity-10" />
-              </div>
-
-              <div className="absolute flex justify-center bottom-0 mb-3">
-                <div className="flex bg-white px-4 py-1 space-x-5 rounded-lg overflow-hidden shadow">
-                  <p className="flex items-center font-medium text-gray-800">
-                    4G
-                  </p>
-
-                  <p className="flex items-center font-medium text-gray-800">
-                    GPS
-                  </p>
-
-                  <p className="flex items-center font-medium text-gray-800">
-                    LCD
-                  </p>
-                </div>
-              </div>
-              <span className="absolute top-0 left-0 inline-flex mt-3 ml-3 px-3 py-2 rounded-lg z-10 bg-grey2 text-sm font-medium text-white select-none">
-                Rupture de stock
-              </span>
+        <div className="shadow p-4 rounded-lg bg-white">
+          <div className="flex justify-center relative rounded-lg overflow-hidden h-90">
+            <div className="transition-transform duration-500 transform ease-in-out hover:scale-110 w-full">
+              <img
+                src="https://www.indicereparabilite.fr/wp-content/uploads/2022/01/8-a-10-2.png"
+                alt="Descripción de la imagen"
+              />
+              <div className="absolute inset-0 bg-black opacity-10" />
             </div>
 
-            <div className="text-center block mt-4">
-              <h2
-                className="font-medium text-base md:text-lg text-gray-800 line-clamp-1"
-                title="New York"
-              >
-                Smartphone Spectralink
-              </h2>
-              <p
-                className="mt-2 text-sm text-red line-clamp-1"
-                title="New York, NY 10004, United States"
-              >
-                INDICE DE RÉPARABILITÉ SMARTPHONE
-              </p>
-            </div>
+            <div className="absolute flex justify-center bottom-0 mb-3">
+              <div className="flex bg-white px-4 py-1 space-x-5 rounded-lg overflow-hidden shadow">
+                <p className="flex items-center font-medium text-gray-800">
+                  4G
+                </p>
 
-            <div className=" mt-8">
-              <div className="flex items-center">
-                <button
-                  type="button"
-                  className="bg-main hover:bg-main-light text-white py-2 px-4 rounded-full "
-                >
-                  Voir plus
-                </button>
+                <p className="flex items-center font-medium text-gray-800">
+                  GPS
+                </p>
+
+                <p className="flex items-center font-medium text-gray-800">
+                  LCD
+                </p>
               </div>
             </div>
+            <span className="absolute top-0 left-0 inline-flex mt-3 ml-3 px-3 py-2 rounded-lg z-10 bg-grey2 text-sm font-medium text-white select-none">
+              Rupture de stock
+            </span>
           </div>
-        </a>
+
+          <div className="text-center block mt-4">
+            <h2
+              className="font-medium text-base md:text-lg text-gray-800 line-clamp-1"
+              title="New York"
+            >
+              Smartphone Spectralink
+            </h2>
+            <p
+              className="mt-2 text-sm text-red line-clamp-1"
+              title="New York, NY 10004, United States"
+            >
+              INDICE DE RÉPARABILITÉ SMARTPHONE
+            </p>
+          </div>
+        </div>
       </div>
       <div className="relative mx-auto w-full">
-        <a
-          href="/"
-          className="relative inline-block duration-300 ease-in-out transition-transform transform hover:-translate-y-2 w-full"
-        >
-          <div className="shadow p-4 rounded-lg bg-white">
-            <div className="flex justify-center relative rounded-lg overflow-hidden h-90">
-              <div className="transition-transform duration-500 transform ease-in-out hover:scale-110 w-full">
-                <img
-                  src="https://www.indicereparabilite.fr/wp-content/uploads/2021/01/6-a-79.png"
-                  alt="Descripción de la imagen"
-                />
-                <div className="absolute inset-0 bg-black opacity-10" />
-              </div>
-
-              <div className="absolute flex justify-center bottom-0 mb-3">
-                <div className="flex bg-white px-4 py-1 space-x-5 rounded-lg overflow-hidden shadow">
-                  <p className="flex items-center font-medium text-gray-800">
-                    4G
-                  </p>
-
-                  <p className="flex items-center font-medium text-gray-800">
-                    GPS
-                  </p>
-
-                  <p className="flex items-center font-medium text-gray-800">
-                    LCD
-                  </p>
-                </div>
-              </div>
-
-              <span className="absolute top-0 left-0 inline-flex mt-3 ml-3 px-3 py-2 rounded-lg z-10 bg-green text-sm font-medium text-white select-none">
-                Disponible
-              </span>
+        <div className="shadow p-4 rounded-lg bg-white">
+          <div className="flex justify-center relative rounded-lg overflow-hidden h-90">
+            <div className="transition-transform duration-500 transform ease-in-out hover:scale-110 w-full">
+              <img
+                src="https://www.indicereparabilite.fr/wp-content/uploads/2021/01/6-a-79.png"
+                alt="Descripción de la imagen"
+              />
+              <div className="absolute inset-0 bg-black opacity-10" />
             </div>
 
-            <div className="text-center block mt-4">
-              <h2
-                className="font-medium text-base md:text-lg text-gray-800 line-clamp-1"
-                title="New York"
-              >
-                Smartphone OnePlus 8T
-              </h2>
-              <p
-                className="mt-2 text-sm text-cyan-500 line-clamp-1"
-                title="New York, NY 10004, United States"
-              >
-                INDICE DE RÉPARABILITÉ SMARTPHONE
-              </p>
-            </div>
+            <div className="absolute flex justify-center bottom-0 mb-3">
+              <div className="flex bg-white px-4 py-1 space-x-5 rounded-lg overflow-hidden shadow">
+                <p className="flex items-center font-medium text-gray-800">
+                  4G
+                </p>
 
-            <div className=" mt-8">
-              <div className="flex items-center">
-                <button
-                  type="button"
-                  className="bg-main hover:bg-main-light text-white py-2 px-4 rounded-full "
-                >
-                  Voir plus
-                </button>
+                <p className="flex items-center font-medium text-gray-800">
+                  GPS
+                </p>
+
+                <p className="flex items-center font-medium text-gray-800">
+                  LCD
+                </p>
               </div>
             </div>
+
+            <span className="absolute top-0 left-0 inline-flex mt-3 ml-3 px-3 py-2 rounded-lg z-10 bg-grey2 text-sm font-medium text-white select-none">
+              Rupture de stock
+            </span>
           </div>
-        </a>
+
+          <div className="text-center block mt-4">
+            <h2
+              className="font-medium text-base md:text-lg text-gray-800 line-clamp-1"
+              title="New York"
+            >
+              Smartphone OnePlus 8T
+            </h2>
+            <p
+              className="mt-2 text-sm text-cyan-500 line-clamp-1"
+              title="New York, NY 10004, United States"
+            >
+              INDICE DE RÉPARABILITÉ SMARTPHONE
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
